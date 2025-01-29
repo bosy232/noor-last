e06ced8c28ab10a5f382a044c5ea97a7b0c56cd0
bosy232 <tarekalashry59@gmail.com>
2025-01-30 01:52:51 +0200

    Update AboutUs.tsx

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
     import { useTranslation } from 'react-i18next';
-import teems.jpg from '../assets/images/teems.jpg';
+import teems from '../assets/images/teems.jpg';
 
 
     const AboutUs = () => {
